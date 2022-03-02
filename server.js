851df544991a5a92abb823e8c55d3249311a523e
Paulin winston <paulinkladi@gmail.com>
2022-03-02 20:50:34 +0000

    third debugging

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(cors());
 app.use("/contact", contactRoute);
 
 if (process.env.NODE_ENV === "production") {  
-  app.use(express.static(  path.join(__dirname, 'build')  ));
+  app.use(express.static("client/build"));
   app.get("/*", (req, res) =>
-    res.sendFile( path.join(__dirname, 'build', 'index.html')  )
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
